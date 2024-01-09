83866cbf01c6d1f493e6b03b5b8b3c0c6eec6f0f
seandonnelly92 <sean_donnelly@hotmail.co.uk>
2024-01-09 22:51:01 +0000

    Created validation alerts for user input

--- script.js
+++ script.js
@@ -89,44 +89,47 @@ var upperCasedCharacters = [
 ];
 
 var potentialCharacters = [] // Defining a new array that will later contain all possible characters of the password.
+var noOfCharacters = 8;
 
 // Function to prompt user for password options
 function passwordPrompts() {
   var potentialCharacters = [] 
 
-  var noOfCharacters = prompt("How many characters should your password have? Choose between 8 and 128 characters.") 
-  
+  var noOfCharacters = prompt("How many characters should your password have? Choose between 8 and 128 characters."); 
+  console.log(noOfCharacters);
+
   if (isNaN(noOfCharacters) || noOfCharacters < 8 || noOfCharacters > 128) { // Validation: if it not a number or less than 8 or more than 128...
     alert("You must input a number between 8 and 128");
-    passwordPrompts() // This re-calls the function to start it again if the invalid conditions are met.
+    passwordPrompts(); // This re-calls the function to start it again if the invalid conditions are met.
     return;
   }
 
   var includeSpecialCharacters = confirm("Would you like to include special characters in your password? Choose 'OK' for yes and 'Cancel' for no.")
   if (includeSpecialCharacters) {
     potentialCharacters = [...specialCharacters];
-    console.log(potentialCharacters)
-
+    console.log(potentialCharacters);
+    console.log(noOfCharacters);
   }
 
   var includeNumbers = confirm("Would you like to include numbers in your password? Choose 'OK' for yes and 'Cancel' for no.")
   if (includeNumbers) {
     potentialCharacters = [...potentialCharacters, ...numericCharacters];
-    console.log(potentialCharacters)
-
+    console.log(potentialCharacters);
+    console.log(noOfCharacters);
   }
 
   var includeLowerCase = confirm("Would you like to include lower case letters in your password? Choose 'OK' for yes and 'Cancel' for no.")
   if (includeLowerCase) {
     potentialCharacters = [...potentialCharacters, ...lowerCasedCharacters];
-    console.log(potentialCharacters)
-
+    console.log(potentialCharacters);
+    console.log(noOfCharacters);
   }
   
   var includeUpperCase = confirm("Would you like to include upper case letters in your password? Choose 'OK' for yes and 'Cancel' for no.")
   if (includeUpperCase) {
     potentialCharacters = [...potentialCharacters, ...upperCasedCharacters];
-    console.log(potentialCharacters)
+    console.log(potentialCharacters);
+    console.log(noOfCharacters);
   }
 
   if (!includeSpecialCharacters && !includeNumbers && !includeLowerCase && !includeUpperCase) {
@@ -134,37 +137,35 @@ function passwordPrompts() {
     passwordPrompts(); // This re-calls the function to start it again. I may go back and make it start from the second prompt by nesting a new function.
     return;
   }
+console.log(noOfCharacters)
 }
 
-// Function to generate password with user input
-function generatePassword() {
-  passwordPrompts()
-
+// Function to select X random characters from the potentialCharacters to form the password
+  
   function generateRandomPassword(potentialCharacters, noOfCharacters) {
-    var randomPassword = "";
-    for (let i = 0; i < noOfCharacters; i++) { // Loops through each index of my potentialCharacters array
+    let randomPassword = "";
+    for (let i = 0; i < noOfCharacters; i++) { // Loops through the number of characters we have
       const randomIndex = Math.floor(Math.random() * potentialCharacters.length); // Declares new variable called randomIndex which is a random index from the array
-      // Math.random gives a random float between 0 and 1 e.g. 0.27. The array length is 85. So it could be 0.27*85 = 22.95. Math.floor rounds it down. So it would be 22. Index 22 would be chosen. 
+      // Math.random gives a random float between 0 and 1 e.g. 0.27. The max array length is 85 if all character types are accepted. So it could be 0.27*85 = 22.95. Math.floor rounds it down. So it would be 22. Index 22 would be chosen. 
       randomPassword += potentialCharacters[randomIndex];   // Append the selected character to the password. += is the compound assignment operator for addition and assignment. It adds the value on the right-hand side to the variable on the left-hand side and then assigns the result back to the variable.
     }
-    return randomPassword
-    console.log(randomPassword) 
+    return randomPassword;
   }
-  
 
-}
+
+  var newpassword = generateRandomPassword(); // calling the generatePassword function and storing the resulting value in this new password variable.
+  console.log(newpassword); // Console logging this new variable's value 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate'); 
-// Declaring a new variable that is the button in the
-// html doc. 
+var generateBtn = document.querySelector('#generate');  // Creates a new var which is the html document's button with the id #generate
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); // calling the generatePassword function and storing the resulting value in this password variable.
-  var passwordText = document.querySelector('#password'); // declare new variable to reference the placeholder text
-
-  passwordText.value = password; // 
+  passwordPrompts();
+  var password = generateRandomPassword(); // calling the generatePassword function and storing the resulting value in this new password variable.
+  console.log(password); // Console logging this new variable's value 
+  var passwordText = document.querySelector('#password'); // declare new variable which is filled with the placeholder text
+  passwordText.value = password; // Overwriting the placeholder text with the generated password 
 }
 
 // Add event listener to generate button
