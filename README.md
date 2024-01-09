8210d8d77f622fec763c732c79c89058516c1b8a
seandonnelly92 <sean_donnelly@hotmail.co.uk>
2024-01-09 23:46:36 +0000

    Wrote readme, removed console logs, improved formatting

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Readme here 
+_This project was created for module 5 of the EdX Skills for Life Front End Development bootcamp. In this challenge, we were tasked with creating a password generator._
+
+
+# Password Generator
+
+## Description
+
+This simple password generator lets you choose a password length between 8 and 128 characters long and which character types should be included out of special characters, numbers, lower case and upper case characters. It does this by putting all of the potential characters into an array, then looping through generating random selections from this array of characters and adding as many as your chosen password length to the generated password. 
+
+**Here is the published website URL:** https://seandonnelly92.github.io/password-generator/
+
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+
+You can see the live website by visting https://seandonnelly92.github.io/password-generator/
+
+If you want to work on the software locally, here is one way of doing it
+
+1. Go to https://github.com/seandonnelly92/password-generator/ 
+2. Download the code repository by clicking the green 'Code' button and then 'Download Zip' as shown here
+
+    ![GIF showing how to download the code repository from GitHub](images/download-repository.gif)
+
+3. Navigate to your downloads folder an Unzip the repository folder. 
+4. Move whole folder to a location where you want your project to be stored locally.
+5. Open Visual Studio Code
+6. Click File > Open Folder... 
+7. Navigate to the location where you stored the repository
+8. Select the folder
+9. Click Open
+10. Now you can work on the codebase locally! 
+
+## Usage
+
+As outlined above, this password generator will allow you create randomised passwords that meet chosen criteria. 
+
+
+## Credits
+
+Thank you to EdX Skills for Life team including Abdul and all of the TAs for their support so far. 
+
+
+## License
+
+MIT License
+
+Copyright (c) 2023 seandonnelly92
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,233 +1,177 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-var potentialCharacters = [] // Defining a new array that will later contain all possible characters of the password.
+var potentialCharacters = []; // Defining a new array that will later contain all possible characters of the password.
 var noOfCharacters = 8;
 
 // Function to prompt user for password options
 function passwordPrompts() {
   potentialCharacters = [];
-  var noOfCharacters = prompt("How many characters should your password have? Choose between 8 and 128 characters."); 
-  console.log(noOfCharacters);
+  var noOfCharacters = prompt(
+    "How many characters should your password have? Choose between 8 and 128 characters."
+  );
 
-  if (isNaN(noOfCharacters) || noOfCharacters < 8 || noOfCharacters > 128) { // Validation: if it not a number or less than 8 or more than 128...
+  if (isNaN(noOfCharacters) || noOfCharacters < 8 || noOfCharacters > 128) {
+    // Validation: if it not a number or less than 8 or more than 128...
     alert("You must input a number between 8 and 128");
     passwordPrompts(); // This re-calls the function to start it again if the invalid conditions are met.
     return;
   }
 
-  var includeSpecialCharacters = confirm("Would you like to include special characters in your password? Choose 'OK' for yes and 'Cancel' for no.")
+  var includeSpecialCharacters = confirm(
+    "Would you like to include special characters in your password? Choose 'OK' for yes and 'Cancel' for no."
+  );
   if (includeSpecialCharacters) {
     potentialCharacters = [...specialCharacters];
-    console.log(potentialCharacters);
-    console.log(noOfCharacters);
   }
 
-  var includeNumbers = confirm("Would you like to include numbers in your password? Choose 'OK' for yes and 'Cancel' for no.")
+  var includeNumbers = confirm(
+    "Would you like to include numbers in your password? Choose 'OK' for yes and 'Cancel' for no."
+  );
   if (includeNumbers) {
     potentialCharacters = [...potentialCharacters, ...numericCharacters];
-    console.log(potentialCharacters);
-    console.log(noOfCharacters);
   }
 
-  var includeLowerCase = confirm("Would you like to include lower case letters in your password? Choose 'OK' for yes and 'Cancel' for no.")
+  var includeLowerCase = confirm(
+    "Would you like to include lower case letters in your password? Choose 'OK' for yes and 'Cancel' for no."
+  );
   if (includeLowerCase) {
     potentialCharacters = [...potentialCharacters, ...lowerCasedCharacters];
-    console.log(potentialCharacters);
-    console.log(noOfCharacters);
-  }
-  
-  var includeUpperCase = confirm("Would you like to include upper case letters in your password? Choose 'OK' for yes and 'Cancel' for no.")
-  if (includeUpperCase) {
-    potentialCharacters = [...potentialCharacters, ...upperCasedCharacters];
-    console.log(potentialCharacters);
-    console.log(noOfCharacters);
   }
 
-  if (!includeSpecialCharacters && !includeNumbers && !includeLowerCase && !includeUpperCase) {
+  var includeUpperCase = confirm(
+    "Would you like to include upper case letters in your password? Choose 'OK' for yes and 'Cancel' for no."
+  );
+  if (includeUpperCase) {
+    potentialCharacters = [...potentialCharacters, ...upperCasedCharacters];
+  }
+
+  if (
+    !includeSpecialCharacters &&
+    !includeNumbers &&
+    !includeLowerCase &&
+    !includeUpperCase
+  ) {
     alert("You must select at least one character type.");
     passwordPrompts(); // This re-calls the function to start it again. I may go back and make it start from the second prompt by nesting a new function.
     return;
   }
-console.log(noOfCharacters)
 }
 
 // Function to select X random characters from the potentialCharacters to form the password
-  
-  function generateRandomPassword(potentialCharacters, noOfCharacters) {
-    let randomPassword = "";
-    for (let i = 0; i < noOfCharacters; i++) { // Loops through the number of characters we have
-      const randomIndex = Math.floor(Math.random() * potentialCharacters.length); // Declares new variable called randomIndex which is a random index from the array
-      // Math.random gives a random float between 0 and 1 e.g. 0.27. The max array length is 85 if all character types are accepted. So it could be 0.27*85 = 22.95. Math.floor rounds it down. So it would be 22. Index 22 would be chosen. 
-      randomPassword += potentialCharacters[randomIndex];   // Append the selected character to the password. += is the compound assignment operator for addition and assignment. It adds the value on the right-hand side to the variable on the left-hand side and then assigns the result back to the variable.
-    }
-    return randomPassword;
+
+function generateRandomPassword(potentialCharacters, noOfCharacters) {
+  let randomPassword = "";
+  for (let i = 0; i < noOfCharacters; i++) {
+    // Loops through the number of characters we have
+    const randomIndex = Math.floor(Math.random() * potentialCharacters.length); // Declares new variable called randomIndex which is a random index from the array
+    // Math.random gives a random float between 0 and 1 e.g. 0.27. The max array length is 85 if all character types are accepted. So it could be 0.27*85 = 22.95. Math.floor rounds it down. So it would be 22. Index 22 would be chosen.
+    randomPassword += potentialCharacters[randomIndex]; // Append the selected character to the password. += is the compound assignment operator for addition and assignment. It adds the value on the right-hand side to the variable on the left-hand side and then assigns the result back to the variable.
   }
+  return randomPassword;
+}
 
-
-  var newpassword = generateRandomPassword(potentialCharacters, noOfCharacters); // calling the generatePassword function and storing the resulting value in this new password variable.
-  console.log(newpassword); // Console logging this new variable's value 
+var newpassword = generateRandomPassword(potentialCharacters, noOfCharacters); // calling the generatePassword function and storing the resulting value in this new password variable.
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');  // Creates a new var which is the html document's button with the id #generate
+var generateBtn = document.querySelector("#generate"); // Creates a new var which is the html document's button with the id #generate
 
 // Write password to the #password input
 function writePassword() {
   passwordPrompts();
   var password = generateRandomPassword(potentialCharacters, noOfCharacters); // calling the generatePassword function and storing the resulting value in this new password variable.
-  console.log(password); // Console logging this new variable's value 
-  var passwordText = document.querySelector('#password'); // declare new variable which is filled with the placeholder text
-  passwordText.value = password; // Overwriting the placeholder text with the generated password 
+  var passwordText = document.querySelector("#password"); // declare new variable which is filled with the placeholder text
+  passwordText.value = password; // Overwriting the placeholder text with the generated password
   potentialCharacters = []; // emptying the potential characters array so that the password generator can be re-used
   noOfCharacters = 0; // Resetting noOfCharacters
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-
-
-
-
-
-
-// For each tool in our tool belt: provide the name and what it's good for
-
-// 1. Variables 
-// 	1. Good for naming things so that we can use them later
-// 2. Constant
-// 	1. For naming variables whose value won't change 
-// 3. Strings
-// 	1. collections of characters
-// 4. String Methods
-// 	5. Make changes to strings like toUpper to Lower
-// 5. For loops
-// 	1. iterating through data, when you want to do something to each thing
-// 6. Alerts
-// 	1. Pop up to give the user information OR alert them! or warn them. 
-// 7. Arrays --- COLLECTION
-// 	1. For collecting a list of things
-// 	2. keeps your order! (we use index)
-// 8. Array Methods
-// 	1. When I want to do things to array, slice, pop, splice, sort, etc
-// 9. Iterative Methods
-// 	1. Map, for each when I want to do something to each element in the area 
-// 10. Functions
-// 	1. a task that can be done multiple times, helps you avoid repetition 
-// 11. Function arguments
-// 	1. good for when we want to give functions an input 
-// 12. Methods
-// 	1. a task that can be done multiple times THAT LIVES inside an object, helps you avoid repetition 
-// 13. This
-// 	1. `this` inside of a method is used to references the `properties` and `methods` of the object we're inside. 
-// 	2. really good at selecting things inside of some scope. 
-// 	3. inside of object, this refers to the object. in global scope, it refers to window. 
-// 14. Objects --- COLLECTION
-// 	1. good for collecting information about a THING (object). An account, a password, anything in the real world. 
-// 	2. Get things by their key. Order doesn't matter. 
-// 15. pseudo-code
-// 	1. helps you write down a blueprint before you do the actual work itself. helps you not waste time.
-// 16. Console.log
-// 	1. print the output of whatever we're coding
-// 17. mathematical and comparison operators  
-// 	1. comparing things in conditions
-// 	2. to do maths 
-// 18. Conditional statements (if else)
-// 	1. control your logic 
-// 19. Switch statement 
-// 	1. good for matching on a value 
-// 20. Scope
-// 	1. good for understanding where a variable is available (local or global!)
-// 21. debugger
-// 	1. good for verifying bugs + weird behaviour 
-// 22. Prompts
-// 	1. Good for getting the user's input 
-// 23. Booleans
-// 	1. good for figuring out if things are true OR false 
+generateBtn.addEventListener("click", writePassword);
